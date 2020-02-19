afb58c5abcac6b506d0aca0069cb558c9ac6440a
david.peek@snhu.edu <david.peek@snhu.edu>
2020-02-19 13:32:00 -0500

    Initial commit
    Test plane with walls, cubes, spheres, and a reset button.

---
+++ Assets/Scripts/ResetButtonPush.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetButtonPush : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetButton()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ResetButton();
+        }
+    }
+}
