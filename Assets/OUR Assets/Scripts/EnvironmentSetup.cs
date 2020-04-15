59831d3c3c67daafbad51f64718b722738825023
Mitchell7583 <lamchniho@gmail.com>
2020-04-15 07:26:34 -0400

    attemped to match lobby to real world

--- Assets/OUR Assets/Scripts/EnvironmentSetup.cs
+++ Assets/OUR Assets/Scripts/EnvironmentSetup.cs
@@ -5,21 +5,11 @@ using UnityEngine.UI;
 
 public class EnvironmentSetup : MonoBehaviour
 {
-    private GameObject Environment;
-    public GameObject Room;
-    public GameObject Cornor;
+    public GameObject Environment;
     public bool modify;
-    private bool RoomSpawned = false;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         OVRInput.Update();
 
@@ -43,26 +33,12 @@ public class EnvironmentSetup : MonoBehaviour
         if (OVRInput.Get(OVRInput.Button.One)) //Right Button A
         {
             Vector3 rPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
-            //spawn room on first time
-            if (!RoomSpawned)
-            {
-                Environment = new GameObject("Room");
-                SpawnRoom(new Vector3(rPosition.x - Cornor.transform.position.x, Environment.transform.position.y, rPosition.z - Cornor.transform.position.z));
-                RoomSpawned = true;
-            }
             //set floor height
             Environment.transform.position = new Vector3(Environment.transform.position.x, rPosition.y - 0.1f, Environment.transform.position.z);
-            Environment.SetActive(true);
         }
         if(OVRInput.GetUp(OVRInput.Button.One))
         {
             modify = false;
-            return;
         }
-    }
-
-    void SpawnRoom(Vector3 pos)
-    {
-        Environment = Instantiate(Room, pos, Quaternion.identity) as GameObject;
     }
 }
