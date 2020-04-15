eef6c5b9e953acbbb0ff2d519a71ce805b1785cc
Mitchell7583 <lamchniho@gmail.com>
2020-04-15 01:40:51 -0400

    Now u can draw on WhiteBoard

---
+++ Assets/OUR Assets/Scripts/Drawing.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawing : MonoBehaviour
+{
+    public Material color;
+    public GameObject penSize;
+
+    private void Start()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.name == "WhiteBoard")
+        {
+            Instantiate(penSize, new Vector3(other.transform.position.x, penSize.transform.position.y, penSize.transform.position.z), Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "WhiteBoard")
+        {
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "WhiteBoard")
+        {
+            
+        }
+    }
+}
--- Assets/OUR Assets/Scripts/EnvironmentSetup.cs
+++ Assets/OUR Assets/Scripts/EnvironmentSetup.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class EnvironmentSetup : MonoBehaviour
 {
-    public GameObject Environment;
+    private GameObject Environment;
     public GameObject Room;
-    public bool modify = false;
-    public Text cornerPosition;
+    public GameObject Cornor;
+    public bool modify;
     private bool RoomSpawned = false;
 
 
@@ -23,11 +23,12 @@ public class EnvironmentSetup : MonoBehaviour
     {
         OVRInput.Update();
 
-        if (Environment.GetComponent<ModifyMode>().mode == ModifyMode.ModifyModes.Measurement) //(modify)
+        if (OVRInput.IsControllerConnected(OVRInput.Controller.RTouch))
         {
-            SetFloorLevel();
-            //SetRoomCorner();
-            //SetRoomRotation();
+            if (modify)
+            {
+                InitRoom();
+            }
         }
     }
 
@@ -37,52 +38,26 @@ public class EnvironmentSetup : MonoBehaviour
         Debug.Log("modify=" + modify);
     }
 
-    void SetFloorLevel()
+    void InitRoom()
     {
         if (OVRInput.Get(OVRInput.Button.One)) //Right Button A
         {
             Vector3 rPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
-            //set floor height
-            Environment.transform.position = new Vector3(Environment.transform.position.x, rPosition.y - 0.1f, Environment.transform.position.z);
             //spawn room on first time
             if (!RoomSpawned)
             {
-                SpawnRoom(new Vector3(rPosition.x, Environment.transform.position.y, rPosition.z));
+                Environment = new GameObject("Room");
+                SpawnRoom(new Vector3(rPosition.x - Cornor.transform.position.x, Environment.transform.position.y, rPosition.z - Cornor.transform.position.z));
                 RoomSpawned = true;
             }
+            //set floor height
+            Environment.transform.position = new Vector3(Environment.transform.position.x, rPosition.y - 0.1f, Environment.transform.position.z);
         }
-    }
-
-    void SetRoomRotation()
-    {
-        if(OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft))
+        if(OVRInput.GetUp(OVRInput.Button.One))
         {
-            Environment.transform.Rotate(0, Environment.transform.rotation.y - 0.5f, 0, Space.World);
+            modify = false;
+            return;
         }
-        else if(OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight))
-        {
-            Environment.transform.Rotate(0, Environment.transform.rotation.y + 0.5f, 0, Space.World);
-        }
-    }
-
-    void SetRoomCorner()
-    {
-        Vector3 lPosition = new Vector3(0, 0, 0);
-        Vector3 rPosition = new Vector3(0, 0, 0);
-        if (OVRInput.Get(OVRInput.Button.Four)) //Left Button B
-        {
-            lPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
-        }
-        if (OVRInput.Get(OVRInput.Button.Two)) //Right Button B
-        {
-            rPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
-        }
-        if (lPosition != new Vector3(0,0,0) && rPosition != new Vector3(0, 0, 0))
-        {
-            //set corner
-            //Environment.transform.position = new Vector3(lPosition.x + 0.05f, Environment.transform.position.y, rPosition.z - 0.05f);
-        }
-
     }
 
     void SpawnRoom(Vector3 pos)
